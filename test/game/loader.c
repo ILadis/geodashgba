3bcb50b49f7739eafd4dd6fe6a6405f23379ad29
Michael Wagner <mwagn4@gmail.com>
2022-12-21 17:19:59 +0100

    Fixes test cases

--- test/game/loader.c
+++ test/game/loader.c
@@ -2,65 +2,36 @@
 #include <game/loader.h>
 #include "../test.h"
 
-test(LoadCourse_ShouldSetExpectedCourseSize) {
-  // arrange
-  Course *course = Course_GetInstance();
-  Loader loader = Loader_ForLayout(""
-    "        |"
-    "        |"
-    "        |");
-
-  // act
-  Loader_LoadCourse(&loader, course);
-
-  // assert
-  assert(loader.size.x == 9);
-  assert(loader.size.y == 3);
-}
-
-test(LoadCourse_ShouldSetExpectedCourseFloorHeight) {
-  // arrange
-  Course *course = Course_GetInstance();
-  Loader loader = Loader_ForLayout(""
-    "        |"
-    "        |"
-    "        |"
-    "        |"
-    "________|");
-
-  // act
-  Loader_LoadCourse(&loader, course);
-
-  // assert
-  assert(course->floor == 9);
-}
-
 test(LoadCourse_ShouldCreatedBoxesAtExpectedPositions) {
   // arrange
-  Course *course = Course_GetInstance();
-  Loader loader = Loader_ForLayout(""
-    "x     x|"
-    "   x   |"
-    "x_____x|");
+  const int offset = 432;
+  static const char *layout[] = {
+    "x     x",
+    "   x   ",
+    "x_____x",
+  };
+
+  Loader loader = Loader_ForLayout(layout);
+
+  Chunk chunk = {0};
+  Chunk_AssignIndex(&chunk, 0),
 
   // act
-  Loader_LoadCourse(&loader, course);
+  Loader_GetChunk(&loader, &chunk);
 
   // assert
-  assert(course->count == 5);
-  assert(course->objects[0].hitbox.center.x ==  8);
-  assert(course->objects[0].hitbox.center.y ==  8);
-  assert(course->objects[1].hitbox.center.x ==104);
-  assert(course->objects[1].hitbox.center.y ==  8);
-  assert(course->objects[2].hitbox.center.x == 56);
-  assert(course->objects[2].hitbox.center.y == 24);
-  assert(course->objects[3].hitbox.center.x ==  8);
-  assert(course->objects[3].hitbox.center.y == 40);
-  assert(course->objects[4].hitbox.center.x ==104);
-  assert(course->objects[4].hitbox.center.y == 40);
+  assert(chunk.count == 5);
+  assert(chunk.objects[0].hitbox.center.x ==   8);
+  assert(chunk.objects[0].hitbox.center.y ==   8 + offset);
+  assert(chunk.objects[1].hitbox.center.x == 104);
+  assert(chunk.objects[1].hitbox.center.y ==   8 + offset);
+  assert(chunk.objects[2].hitbox.center.x ==  56);
+  assert(chunk.objects[2].hitbox.center.y ==  24 + offset);
+  assert(chunk.objects[3].hitbox.center.x ==   8);
+  assert(chunk.objects[3].hitbox.center.y ==  40 + offset);
+  assert(chunk.objects[4].hitbox.center.x == 104);
+  assert(chunk.objects[4].hitbox.center.y ==  40 + offset);
 }
 
 suite(
-  LoadCourse_ShouldSetExpectedCourseSize,
-  LoadCourse_ShouldSetExpectedCourseFloorHeight,
   LoadCourse_ShouldCreatedBoxesAtExpectedPositions);
