e93daabb589e0cc6ebc077e88052aa37e13cd395
Michael Wagner <mwagn4@gmail.com>
2023-05-02 00:22:33 +0200

    Adds directory traversal

--- include/disk.h
+++ include/disk.h
@@ -7,61 +7,45 @@ typedef bool (*Disk_ReadFn)(unsigned int sector, void *buffer);
 
 typedef struct Disk {
   unsigned char buffer[512];
+  unsigned int offset;
   bool (*read)(unsigned int sector, void *buffer);
-  struct {
-    unsigned short bytesPerSector;
-    unsigned short sectorsPerCluster;
-    unsigned short numberOfReservedSectors;
-    unsigned short numberOfFATs;
-    unsigned short numberOfSectorsPerFAT;
-    unsigned short clusterOfRootDir;
-  } meta;
+  unsigned short bytesPerSector;
+  unsigned short sectorsPerCluster;
+  unsigned short numberOfReservedSectors;
+  unsigned short numberOfFATs;
+  unsigned short numberOfSectorsPerFAT;
+  unsigned short clusterOfRootDir;
+  unsigned int rootDirectory;
 } Disk;
 
-static inline bool
-Disk_BufferFill(Disk *disk, unsigned int sector) {
-  return disk->read(sector, disk->buffer);
-}
-
-static inline bool
-Disk_BufferCompare(Disk *disk, unsigned int offset, char *data) {
-  for (int i = 0; data[i] != '\0'; i++) {
-    if (disk->buffer[offset + i] != data[i]) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-static inline unsigned int
-Disk_BufferGetU32(Disk *disk, unsigned int offset) {
-  return 0
-    | (disk->buffer[offset + 0] <<  0)
-    | (disk->buffer[offset + 1] <<  8)
-    | (disk->buffer[offset + 2] << 16)
-    | (disk->buffer[offset + 3] << 24);
-}
-
-static inline unsigned short
-Disk_BufferGetU16(Disk *disk, unsigned int offset) {
-  return 0
-    | (disk->buffer[offset + 0] <<  0)
-    | (disk->buffer[offset + 1] <<  8);
-}
-
-static inline unsigned char
-Disk_BufferGetU8(Disk *disk, unsigned int offset) {
-  return 0
-    | (disk->buffer[offset + 0] <<  0);
-}
-
-static inline void
-Disk_ReadFrom(Disk *disk, Disk_ReadFn read) {
-  disk->read = read;
-}
+typedef struct DiskEntry {
+  char name[11];
+  enum {
+    DISK_ENTRY_FILE,
+    DISK_ENTRY_DIRECTORY,
+  } type;
+  unsigned int startCluster;
+  unsigned int fileSize;
+} DiskEntry;
 
 bool
-Disk_Initialize(Disk *disk);
+Disk_Initialize(
+    Disk *disk,
+    Disk_ReadFn read);
+
+bool
+Disk_OpenDirectory(
+    Disk *disk,
+    char *path[]);
+
+bool
+Disk_ReadDirectory(
+    Disk *disk,
+    DiskEntry *entry);
+
+bool
+DiskEntry_NameEquals(
+    DiskEntry *entry,
+    char *name);
 
 #endif
--- source/disk.c
+++ source/disk.c
@@ -1,29 +1,77 @@
 
 #include <disk.h>
 
+static inline bool
+Disk_BufferFill(Disk *disk, unsigned int sector) {
+  return disk->read(sector, disk->buffer);
+}
+
+static inline bool
+Disk_BufferCompare(
+    Disk *disk,
+    unsigned int offset,
+    char *data)
+{
+  for (int i = 0; data[i] != '\0'; i++) {
+    if (disk->buffer[offset + i] != data[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+static inline unsigned int
+Disk_BufferGetU32At(Disk *disk, unsigned int offset) {
+  return 0
+    | (disk->buffer[offset + 0] <<  0)
+    | (disk->buffer[offset + 1] <<  8)
+    | (disk->buffer[offset + 2] << 16)
+    | (disk->buffer[offset + 3] << 24);
+}
+
+static inline unsigned short
+Disk_BufferGetU16At(Disk *disk, unsigned int offset) {
+  return 0
+    | (disk->buffer[offset + 0] <<  0)
+    | (disk->buffer[offset + 1] <<  8);
+}
+
+static inline unsigned char
+Disk_BufferGetU8At(Disk *disk, unsigned int offset) {
+  return 0
+    | (disk->buffer[offset + 0] <<  0);
+}
+
 bool
-Disk_Initialize(Disk *disk) {
+Disk_Initialize(
+    Disk *disk,
+    Disk_ReadFn read)
+{
+  disk->offset = 0;
+  disk->read = read;
+
   if (!Disk_BufferFill(disk, 0)) {
     return false;
   }
 
   // invalid master boot record
-  if (Disk_BufferGetU16(disk, 0x01FE) != 0xAA55) {
+  if (Disk_BufferGetU16At(disk, 0x01FE) != 0xAA55) {
     return false;
   }
 
-  if (Disk_BufferGetU8(disk, 0x01C2) != 0x0C /*FAT32 with LBA*/) {
+  if (Disk_BufferGetU8At(disk, 0x01C2) != 0x0C /*FAT32 with LBA*/) {
     return false;
   }
 
   // get first partition LBA
-  unsigned int offset = Disk_BufferGetU32(disk, 0x01C6);
+  unsigned int offset = Disk_BufferGetU32At(disk, 0x01C6);
   if (!Disk_BufferFill(disk, offset)) {
     return false;
   }
 
   // invalid super block
-  if (Disk_BufferGetU16(disk, 0x01FE) != 0xAA55) {
+  if (Disk_BufferGetU16At(disk, 0x01FE) != 0xAA55) {
     return false;
   }
 
@@ -32,12 +80,18 @@ Disk_Initialize(Disk *disk) {
     return false;
   }
 
-  disk->meta.bytesPerSector = Disk_BufferGetU16(disk, 0x000B);
-  disk->meta.sectorsPerCluster = Disk_BufferGetU8(disk, 0x000D);
-  disk->meta.numberOfReservedSectors = Disk_BufferGetU16(disk, 0x000E);
-  disk->meta.numberOfFATs = Disk_BufferGetU8(disk, 0x0010);
-  disk->meta.numberOfSectorsPerFAT = Disk_BufferGetU16(disk, 0x0024);
-  disk->meta.clusterOfRootDir = Disk_BufferGetU32(disk, 0x002C);
+  disk->bytesPerSector = Disk_BufferGetU16At(disk, 0x000B);
+  disk->sectorsPerCluster = Disk_BufferGetU8At(disk, 0x000D);
+  disk->numberOfReservedSectors = Disk_BufferGetU16At(disk, 0x000E);
+  disk->numberOfFATs = Disk_BufferGetU8At(disk, 0x0010);
+  disk->numberOfSectorsPerFAT = Disk_BufferGetU16At(disk, 0x0024);
+  disk->clusterOfRootDir = Disk_BufferGetU32At(disk, 0x002C);
+
+  unsigned int rootOffset = offset
+    + disk->numberOfReservedSectors
+    + disk->numberOfFATs * disk->numberOfSectorsPerFAT;
+
+  disk->rootDirectory = rootOffset;
 
 /*
   unsigned int sectorSize = disk->meta.bytesPerSector;
@@ -51,10 +105,92 @@ Disk_Initialize(Disk *disk) {
     + offset * 512
     + disk->meta.numberOfReservedSectors * sectorSize
     + disk->meta.numberOfFATs * disk->meta.numberOfSectorsPerFAT * sectorSize;
-
-  printf("FAT addr at: %x\n", fatAddress);
-  printf("rootDir addr at: %x\n", rootDirAddress);
 */
+
+  return true;
+}
+
+bool
+Disk_OpenDirectory(
+    Disk *disk,
+    char *path[])
+{
+  unsigned int offset = disk->rootDirectory;
+  if (!Disk_BufferFill(disk, offset)) {
+    return false;
+  }
+
+  int index = 0;
+  while (path[index] != NULL) {
+    char *name = path[index++];
+
+    DiskEntry entry = {0};
+    do {
+      if (!Disk_ReadDirectory(disk, &entry)) {
+        return false;
+      }
+
+      if (entry.type != DISK_ENTRY_DIRECTORY) {
+        return false;
+      }
+    } while (!DiskEntry_NameEquals(&entry, name));
+
+    unsigned int offset = disk->rootDirectory + (entry.startCluster - disk->clusterOfRootDir) * disk->sectorsPerCluster;
+    if (!Disk_BufferFill(disk, offset)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+bool
+Disk_ReadDirectory(
+    Disk *disk,
+    DiskEntry *entry)
+{
+  unsigned char *buffer = NULL;
+  unsigned int offset = disk->offset;
+  unsigned char flags = 0;
+
+  next: {
+    buffer = &disk->buffer[offset];
+    if (*buffer == 0x00) {
+      return false;
+    }
+
+    flags = Disk_BufferGetU8At(disk, offset + 11);
+
+    // skip hidden or system files
+    if (flags & 0b00110) {
+      offset += 32;
+      goto next;
+    }
+  }
+
+  for (int i = 0; i < length(entry->name); i++) {
+    entry->name[i] = buffer[i];
+  }
+
+  entry->type = flags & 0b10000 ? DISK_ENTRY_DIRECTORY : DISK_ENTRY_FILE;
+  entry->startCluster = Disk_BufferGetU16At(disk, offset + 26);
+  entry->fileSize = Disk_BufferGetU32At(disk, offset + 28);
+
+  disk->offset = offset + 32;
+
+  return true;
+}
+
+bool
+DiskEntry_NameEquals(
+    DiskEntry *entry,
+    char *name)
+{
+  for (int i = 0; i < length(entry->name); i++) {
+    if (name[i] == '\0' || name[i] != entry->name[i]) {
+      return false;
+    }
+  }
 
   return true;
 }
